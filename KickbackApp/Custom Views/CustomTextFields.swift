cfc069fb62034810aaa4dd7da7747095dab16099
Stefan Hopman <stefan@Stefans-MacBook-Pro.local>
2022-02-18 13:24:33 -0500

    Forgot Password Finished

---
+++ KickbackApp/Custom Views/CustomTextFields.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTextFields.swift
+//  KickbackApp
+//
+//  Created by Stefan Hopman on 2/18/22.
+//
+
+import SwiftUI
+
+struct CustomTextFields: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomTextFields_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomTextFields()
+    }
+}
