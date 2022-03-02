678aa97edbb1debab20bd766ae1c21225c9edfcf
Joga Singh <jogasinghbajwa@gmail.com>
2022-03-02 17:36:56 +0530

    HomeView and ContentView files updated with latest code.

--- KickbackApp/Views/HomeView.swift
+++ KickbackApp/Views/HomeView.swift
@@ -26,32 +26,7 @@ struct HomeView: View {
     @ObservedObject var viewModel: HomeViewModel
     
     var body: some View {
-        VStack {
-            HStack {
-                VStack(alignment: .leading) {
-                    Text ("Hi, lemon")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color(red: 0.203, green: 0.249, blue: 0.534))
-                        .multilineTextAlignment(.leading)
-                    Text("Are you ready to rock'in the class?")
-                        .font(.subheadline)
-                }   /// VStack closed
-                Spacer()
-                Image(systemName: "person.fill")
-                    .resizable()
-                    .frame(width: 20, height: 20)
-                    .padding(10)
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(Color.black, lineWidth: 1))
-                    .shadow(radius: 10)
-                    .onTapGesture {
-                        print("Profile image tapped")
-                    }
-            }.navigationBarHidden(true)    /// HStack closed
-            
-            Spacer()
-                .frame(height: 10)
+        
 //            List(viewModel.courseList) { course in
 //                CourseListView(viewModel: CourseListViewModel(course)).listRowInsets(EdgeInsets())
 //                    .listRowBackground(Color.clear)
@@ -59,35 +34,66 @@ struct HomeView: View {
 //            .listStyle(GroupedListStyle())
             
             
-            List {
+        ScrollView(.vertical, showsIndicators: false) {
+                VStack {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text ("Hi, Jamie")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.black)
+                                .multilineTextAlignment(.leading)
+                            Text("Welcome back!")
+                                .font(.subheadline)
+                                .foregroundColor(Color.kGray)
+                        }   /// VStack closed
+                        Spacer()
+                        Image(systemName: "person.fill")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .padding(10)
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.black, lineWidth: 1))
+                            .shadow(radius: 10)
+                            .onTapGesture {
+                                print("Profile image tapped")
+                            }
+                    }
+                    .navigationBarHidden(true)    /// HStack closed
+                    .padding(.leading, 10)
+                    .padding(.trailing, 10)
+                    
+                    Spacer()
+                        .frame(height: 10)
                 ForEach(viewModel.courseList) { course in
                     CourseListView(viewModel: CourseListViewModel(course))
                         .background(Color.white)
                         .listRowInsets(EdgeInsets())
                 }
-//                .background(Color.white)
-            }
-            .listStyle(GroupedListStyle())
-            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-//            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-//                .listStyle(PlainListStyle())
+                .padding(.bottom, 10)
+                VStack(alignment: .center){
+                    Button(action: {
+                        print("Button Tapped")
+                    }) {
+                        Text("BOOK NOW")
+                            .foregroundColor(.white)
+                            .fontWeight(.bold)
+                            .padding(.vertical)
+                            .padding(.horizontal, 50)
+                            .background(Color.black)
+                            .clipShape(Capsule())
+                            .shadow(color: Color.white.opacity(0.1), radius: 5, x: 0, y: 5)
+                        }
+                    
+                    }
                 
+            }
+                
+            .listStyle(GroupedListStyle())
             
-            Button(action: {
-                    print("sign up bin tapped")
-                }) {
-                    Text("Book Class")
-                        .frame(minWidth: 0)
-                        .font(.system(size: 18))
-                        .padding(EdgeInsets(top: 15, leading: 40, bottom: 15, trailing: 40))
-                        .foregroundColor(.white)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 25)
-                                .stroke(Color.white, lineWidth: 2)
-                    )
-                }
-                .background(Color(red: 0.203, green: 0.249, blue: 0.534)) // If you have this
-                .cornerRadius(25)
+            .padding(EdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 0))
+//            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+//                .listStyle(PlainListStyle()
             
 //            CourseListView(viewModel: .init(testCourseList))
 //            CourseListView(viewModel: .init(testCourseList))
@@ -95,10 +101,14 @@ struct HomeView: View {
 //                CourseListView(viewModel: .init(testCourseList))
 //                CourseListView(viewModel: .init(testCourseList))
 //            }
-            Spacer()
+
                 
-        }.padding(10.0)  /// VStack closed
+        }
+        
+            
+            
     }
+       
 }
 
 struct HomeView_Previews: PreviewProvider {
